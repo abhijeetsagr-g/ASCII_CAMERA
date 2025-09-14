c106368b2e2f303e007605e46861e06a560dd767
abhijeetsagr-g <abhijeetsagarg@gmail.com>
2025-09-15 03:22:03 +0530

    Initial Push

---
+++ scripts/build/CMakeFiles/ascii_algorithm.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vansg/MyProjects/C++/ASCII_CAMERA/scripts/main.cpp" "CMakeFiles/ascii_algorithm.dir/main.cpp.o" "gcc" "CMakeFiles/ascii_algorithm.dir/main.cpp.o.d"
+  "" "ascii_algorithm" "gcc" "CMakeFiles/ascii_algorithm.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ scripts/build/CMakeFiles/ascii_algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascii_algorithm.dir/link.d"
+  "CMakeFiles/ascii_algorithm.dir/main.cpp.o"
+  "CMakeFiles/ascii_algorithm.dir/main.cpp.o.d"
+  "ascii_algorithm"
+  "ascii_algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascii_algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
